27aa971a9dbd5e878eda2ab937f773d98111632b
Arvīds Ivbuls <arvids.ivbuls@esynergy.lv>
2025-05-23 09:03:43 +0300

    seeder adjustments/ swap html with react

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
+        User::firstOrCreate([
             'email' => 'test@example.com',
+        ],[
+            'name' => 'Test User',
         ]);
     }
 }
